f1db02ddcfcd78863ff6074d2bb941fdfdaa07a8
DaimerBaron <daimer.baron@est.iudigital.edu.co>
2025-08-15 22:31:21 -0500

    refactor: restructure App component layout with header and main content

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ function App() {
 
   return (
    <>
-    <p className='bg-red-400'>Hola mundo</p>
+    <header className="bg-blue-600 py-6 h-max-72" >
+      <h1 className="uppercase text-white text-center text-xl font-black">Planificador de gastos</h1>
+    </header>
+    <div className=" max-w-3xl bg-white rounded-lg mx-auto mt-10 shadow-lg p-10">
+      <p>Mi formulario aqui </p>
+    </div>
+
    </>
   )
 }
