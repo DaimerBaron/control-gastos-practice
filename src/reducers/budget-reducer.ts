1d4a456c103ecb59bbecef2d22de7b2bcac819ca
Daimer Baron de los Santos <169172783+DaimerBaron@users.noreply.github.com>
2025-08-18 17:58:34 +0000

    feat: agregar accion para agragar un nuevo gasto

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,14 +1,19 @@
+import { DraftExpense } from "../types"
+
 export type BudgetActions = 
 {type: 'add-budget', payload: {budget:number}}|
-{type: 'show-modal'}|{type: 'close-modal'}
+{type: 'show-modal'}|{type: 'close-modal'}|
+{type: 'add-expense', payload:{expense:DraftExpense}}
 
 export type BudgetState = {
     budget: number
     modal: boolean
+    expense: DraftExpense[]
 }
 export const initialState: BudgetState = {
     budget:0,
-    modal: false
+    modal: false,
+    expense:[]
 }
 
 export const budgetReducer = (state: BudgetState = initialState, action: BudgetActions) =>{
@@ -24,6 +29,12 @@ export const budgetReducer = (state: BudgetState = initialState, action: BudgetA
         case 'close-modal':
             return {
                 ...state, modal:false
+            }
+        case 'add-expense':
+            console.log(state);
+            
+            return {
+                ...state, expense:[...state.expense,action.payload.expense]
             }
         default:
             return state;
